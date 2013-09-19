0852e0be0482a09eeeb7d9ffad96ea6af0fac32f
shinnn <dminor.sw@gmail.com>
2013-09-20 02:41:36 +0900

    auto commit by grunt-github-pages

---
+++ site/debug/js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+if (typeof DEBUG === "undefined" || DEBUG === null) {
+  this.DEBUG = true;
+}
+
+if (DEBUG) {
+  console.log('--- DEBUG MODE ---');
+}
+
+/*
+//@ sourceMappingURL=main.js.map
+*/
---
+++ site/debug/js/main.src.coffee
@@ -0,0 +1,8 @@
+"use strict"
+
+if not DEBUG?
+  this.DEBUG = true
+
+if DEBUG
+  console.log '--- DEBUG MODE ---'
+
