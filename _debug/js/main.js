70e1ca34cd6f9bb76e7a28b98f0c90c7d7ba642c
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-01-08 17:58:50 +0900

    Update 2015-01-08T08:58:47.083Z

--- _debug/js/main.js
+++ _debug/js/main.js
@@ -1,7 +1,7 @@
 /*! MIT (c) Shinnosuke Watanabe */
 (function() {
   'use strict';
-  var $doc, $w, AppView, ContentView, HomeView, NavLabelView, NavPointView, Page, PrevNextButtonView, ProjectImageView, ProjectThumbnailView, Router, ShowMoreCommitsView, TabView, VerticalLineView, VerticalNavView, container, content, hidden, router, visibilityChange, _date;
+  var $doc, $w, AppView, ArticleLabelView, ContentView, GroupLabelView, HomeView, NavPointView, Page, PrevNextButtonView, ProjectImageView, ProjectThumbnailView, Router, ShowMoreCommitsView, TabView, VerticalLineView, VerticalNavView, container, content, hidden, router, visibilityChange, _date;
 
   if (typeof DEBUG === "undefined" || DEBUG === null) {
     this.DEBUG = true;
@@ -91,6 +91,9 @@
     $('.intersection .point > div').css({
       visibility: 'visible'
     });
+    router.on('route', function() {
+      return container.refresh();
+    });
     return container.refresh();
   });
 
@@ -173,12 +176,13 @@
       this.model.on('change:category', (function(_this) {
         return function(model, value) {
           _this.menuTabs.activate(value);
-          return _this.verticalNav.setLink(value);
+          _this.verticalNav.setLink(value);
+          return _this.verticalNav.renderGroup(value);
         };
       })(this));
       this.model.on('change:basename', (function(_this) {
         return function(model, value) {
-          return _this.verticalNav.render(value);
+          return _this.model.set('articleChanged', true);
         };
       })(this));
       return $doc.on(visibilityChange, (function(_this) {
@@ -211,10 +215,79 @@
     }
   });
 
+  ContentView = Backbone.View.extend({
+    el: 'main',
+    hide: function(callback) {
+      NProgress.set(0.0).start();
+      return this.$el.fadeTo(8, 0.010, function() {
+        if (callback) {
+          return callback();
+        }
+      });
+    },
+    show: function(callback) {
+      NProgress.done();
+      this.$el.fadeTo(80, 1);
+      router.prev = Backbone.history.fragment;
+      if (callback) {
+        return callback();
+      }
+    },
+    render: function(page, callback) {
+      if (DEBUG) {
+        page = "/_debug" + page;
+      }
+      return $.ajax({
+        url: page,
+        dataType: "html"
+      }).done((function(_this) {
+        return function(responseText) {
+          document.title = $(responseText).filter('title').text();
+          if (container.model.get('articleChanged')) {
+            container.verticalNav.render(container.model.get('basename'));
+          }
+          _this.$el.html($('<div>').append($.parseHTML(responseText)).find('#content'));
+          return callback();
+        };
+      })(this));
+    }
+  });
+
+  ProjectThumbnailView = Backbone.View.extend({
+    el: 'img[data-original]',
+    render: function() {
+      return this.$el.removeAttr('src')._enableWebP().lazyload({
+        effect: 'fadeIn',
+        threshold: $w.height() * 0.15,
+        placeholder: '/img/transparent.gif'
+      });
+    }
+  });
+
+  HomeView = Backbone.View.extend({
+    reset: function() {
+      $('.wide-image').css('width', '100%');
+      return $('.featured-works').css('visibility', 'visible').fadeTo(750, 1);
+    },
+    render: function() {
+      this.reset();
+      $('img[data-original]').removeAttr('src')._enableWebP().lazyload({
+        effect: 'fadeIn',
+        threshold: $w.height() * 0.25,
+        skip_invisible: false,
+        placeholder: '/img/transparent.gif'
+      });
+      return $('#logo').delay(200).transition({
+        backgroundPosition: '0px 0px'
+      }, 500);
+    }
+  });
+
   VerticalNavView = Backbone.View.extend({
     el: 'header div.intersection',
     initialize: function() {
-      this.labels = new NavLabelView();
+      this.groupLabel = new GroupLabelView();
+      this.articleLabel = new ArticleLabelView();
       this.points = new NavPointView();
       return this.line = new VerticalLineView();
     },
@@ -230,37 +303,67 @@
     },
     render: function(basename) {
       var pageData;
-      this.points.show();
-      if (basename === 'index') {
+      if (basename === 'projects' || basename === 'about') {
+        $('#article-title, #article-info').hide();
+        this.line.extendToFirst();
+      }
+      pageData = routerData[container.model.get('basename')];
+      if (pageData.proj_title) {
+        this.articleLabel.update();
+        this.groupLabel.update('プロジェクト一覧');
+        $('.intersection').show();
+        $('#article-info .time').text(pageData.time);
+        $('#article-info .roles').text(pageData.role.join(' / '));
+        $('#first-point').css({
+          top: "" + ($('#first-location').height() * 0.5) + "px"
+        });
+        $('#article-title-point').css({
+          top: "" + ($('#article-title').height() * 0.5) + "px"
+        });
+        $('#article-info-point').css({
+          top: "" + ($('#article-info').height() * 0.5) + "px"
+        });
+        return this.line.extendToArticleTitle();
+      } else {
+        return this.articleLabel.hide();
+      }
+    },
+    renderGroup: function(groupName) {
+      var pageData;
+      if (groupName === 'index') {
+        $('#article-title, #article-info').fadeOut(42);
         return this.hide();
       } else {
+        this.points.show();
         pageData = routerData[container.model.get('basename')];
         if (pageData.title) {
           $('#first-location').show();
-          $('#first-location .label').text(pageData.title);
-          $('#article-title, #article-info').fadeOut(42);
+          this.groupLabel.update(pageData.title);
           this.line.extendToFirst();
-          $('#first-point').css({
+          return $('#first-point').css({
             top: "" + ($('#first-location').height() * 0.5) + "px"
           });
-        }
-        if (pageData.proj_title) {
-          $('#first-location .label').text('プロジェクト一覧');
-          $('.intersection').show();
-          $('#article-info .time').text(pageData.time);
-          $('#article-info .roles').text(pageData.role.join(' / '));
-          $('#first-point').css({
-            top: "" + ($('#first-location').height() * 0.5) + "px"
-          });
-          $('#article-title-point').css({
-            top: "" + ($('#article-title').height() * 0.5) + "px"
-          });
-          $('#article-info-point').css({
-            top: "" + ($('#article-info').height() * 0.5) + "px"
-          });
-          return this.line.extendToArticleTitle();
         }
       }
+    }
+  });
+
+  GroupLabelView = Backbone.View.extend({
+    el: '#first-location .label',
+    update: function(text) {
+      return this.$el.text(text);
+    }
+  });
+
+  ArticleLabelView = Backbone.View.extend({
+    el: '#article-title > .label',
+    hide: function() {
+      this.$el.fadeOut(30);
+      return this.$el.text('');
+    },
+    update: function() {
+      this.$el.show();
+      return this.$el.text(document.title.replace(/\ -\ .*/, ''));
     }
   });
 
@@ -273,10 +376,6 @@
     show: function() {
       return this.$el.fadeIn();
     }
-  });
-
-  NavLabelView = Backbone.View.extend({
-    el: 'div.intersection > .label'
   });
 
   VerticalLineView = Backbone.View.extend({
@@ -311,91 +410,6 @@
       if (Modernizr.mq('only screen and (min-width: 1280px)')) {
 
       }
-    }
-  });
-
-  ContentView = Backbone.View.extend({
-    el: 'main',
-    hide: function(callback) {
-      NProgress.set(0.0).start();
-      return this.$el.fadeTo(8, 0.010, function() {
-        if (callback) {
-          return callback();
-        }
-      });
-    },
-    show: function(callback) {
-      NProgress.done();
-      this.$el.fadeTo(80, 1);
-      router.prev = Backbone.history.fragment;
-      if (callback) {
-        return callback();
-      }
-    },
-    render: function(page, callback) {
-      if (DEBUG) {
-        page = "/_debug" + page;
-      }
-      return this.$el.load("" + page + " #content", function(responseBody) {
-        var $responseBody, docTitle;
-        $responseBody = $(responseBody);
-        docTitle = $responseBody.filter('title').text();
-        $('#article-title > .label').filter(':visible').text(docTitle.replace(/\ -\ .*/, ''));
-        document.title = docTitle;
-        callback();
-        return container.refresh();
-      });
-    }
-  });
-
-  HomeView = Backbone.View.extend({
-    el: '.top',
-    reset: function() {
-
-      /*
-      $('#scroll-down').fadeOut 80, ->
-        $(this).remove()
-      $('#left-ribbon, #right-ribbon').transition {
-        width: 0
-      }, 250, ->
-        $('#ribbon').remove()
-       */
-      $('.top .inner').transition({
-        paddingTop: '0'
-      }, 400);
-      $('.wide-image').css('width', '100%');
-      return $('.featured-works').css('visibility', 'visible').fadeTo(750, 1);
-    },
-    render: function() {
-      var self;
-      self = this;
-      $w.one('scoll', self.reset());
-      $('#scroll-down').on('click', function() {
-        $('html,body').animate({
-          scrollTop: "" + ($('header').height()) + "px"
-        }, 400);
-        return self.reset();
-      });
-      $('img[data-original]').removeAttr('src')._enableWebP().lazyload({
-        effect: 'fadeIn',
-        threshold: $w.height() * 0.25,
-        skip_invisible: false,
-        placeholder: '/img/transparent.gif'
-      });
-      return $('#logo').delay(200).transition({
-        backgroundPosition: '0px 0px'
-      }, 500);
-    }
-  });
-
-  ProjectThumbnailView = Backbone.View.extend({
-    el: 'img[data-original]',
-    render: function() {
-      return this.$el.removeAttr('src')._enableWebP().lazyload({
-        effect: 'fadeIn',
-        threshold: $w.height() * 0.15,
-        placeholder: '/img/transparent.gif'
-      });
     }
   });
 
